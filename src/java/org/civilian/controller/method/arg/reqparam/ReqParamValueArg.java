15e4f1ee1786cfa985987af7ac9836cdaf83000f
Johannes Döbler <jd@civilian-framework.org>
2023-08-17 10:33:06 +0200

    rename package org.civilian.internal.controller.arg.reqparam to org.civilian.controller.method.arg.reqparam

--- src/java/org/civilian/controller/method/arg/reqparam/ReqParamValueArg.java
+++ src/java/org/civilian/controller/method/arg/reqparam/ReqParamValueArg.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.civilian.internal.controller.arg.reqparam;
+package org.civilian.controller.method.arg.reqparam;
 
 
 import org.civilian.controller.method.arg.StringMethodArg;
