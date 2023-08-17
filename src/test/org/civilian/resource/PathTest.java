49d60cc6ef00c21f1c62dd1269400ab0547327f7
Johannes Döbler <jd@civilian-framework.org>
2023-08-17 17:47:27 +0200

    move to correct package

--- src/test/org/civilian/resource/PathTest.java
+++ src/test/org/civilian/resource/PathTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.civilian.util;
+package org.civilian.resource;
 
 
 import java.io.PrintWriter;
