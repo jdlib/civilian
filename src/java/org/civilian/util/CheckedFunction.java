b3e72f218575a0e6eecafc36bc0fc994fe9bdf18
Johannes Döbler <jd@civilian-framework.org>
2023-08-30 10:53:36 +0200

    add CheckedFunction

---
+++ src/java/org/civilian/util/CheckedFunction.java
@@ -0,0 +1,25 @@
+package org.civilian.util;
+
+
+import java.util.function.Function;
+
+
+/**
+ * Represents a function that accepts one argument and produces a result.
+ * Similar to {@link Function} but allows to throw checked exceptions.
+ * @param <T> the type of the input to the function
+ * @param <R> the type of the result of the function
+ * @param <E> the type of the Throwable
+ */
+@FunctionalInterface
+public interface CheckedFunction<T, R, E extends Throwable> 
+{
+
+    /**
+     * Applies this function to the given argument.
+     *
+     * @param t the function argument
+     * @return the function result
+     */
+    R apply(T t) throws E;
+}
