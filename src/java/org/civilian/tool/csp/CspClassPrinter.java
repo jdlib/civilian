c7692c37d1d63c550940555bfb340b9aac2a33d6
Johannes Döbler <jd@civilian-framework.org>
2023-09-02 18:57:58 +0200

    add CspClassPrinter

---
+++ src/java/org/civilian/tool/csp/CspClassPrinter.java
@@ -0,0 +1,255 @@
+package org.civilian.tool.csp;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import org.civilian.util.Check;
+
+
+/**
+ * Outputs the transpiled Java class from a parsed CSP template. 
+ */
+class CspClassPrinter
+{
+	public CspClassPrinter(SourceWriter out, ClassData classData, boolean printSrcMap)
+	{
+		this.out = Check.notNull(out, "out");
+		classData_ = Check.notNull(classData, "classData");
+		printSrcMap_ = printSrcMap;
+	}
+	
+	
+	public void print(File templFile, boolean timestamp, String body) throws IOException
+	{
+		printClassStart(templFile, timestamp);
+		printBody(body);
+		printFields();
+		out.endBlock(); // class block, started in printClassStart
+	}
+	
+	
+	private void printClassStart(File templFile, boolean timestamp) throws IOException
+	{
+		out.println("/**");
+		out.print(" * Generated from " + templFile.getName());
+		if (timestamp)
+		{
+			out.print(" at ");
+			out.print(LocalDateTime.now());
+		}
+		out.println();
+		out.println(" * Do not edit.");
+		out.println(" */");
+		out.print("package ");
+		out.print(classData_.packageName);
+		out.println(";");
+		out.println();
+		out.println();
+		if (classData_.imports.write(out, null /* app package*/))
+		{
+			out.println();
+			out.println();
+		}
+		for (String prolog : classData_.prolog)
+			out.println(prolog);
+
+		if (classData_.isPublic)
+			out.print("public ");
+		if (classData_.isAbstract)
+			out.print("abstract ");
+		out.print("class ");
+		out.print(classData_.className);
+		if (classData_.extendsClass != null)
+		{
+			out.print(" extends ");
+			out.print(classData_.extendsClass);
+		}
+		if (classData_.implementsList != null)
+		{
+			out.print(" implements ");
+			out.print(classData_.implementsList);
+		}
+		out.println();
+		out.beginBlock();
+
+		if (classData_.needsCtor())
+			printClassCtor(out);
+
+		if (classData_.standalone)
+			printClassPublicPrintMethod(out);
+		if (!classData_.mixins.isEmpty())
+		{
+			printClassInitMethod(out);
+			printClassExitMethod(out);
+		}
+
+		if (classData_.hasMainTemplate)
+		{
+			if (classData_.extendsClass != null)
+				out.print("@Override ");
+			out.print("protected void print()");
+			if (classData_.exception != null)
+			{
+				out.print(" throws ");
+				out.print(classData_.exception);
+			}
+			out.println();
+		}
+	}
+
+	
+	private void printClassCtor(SourceWriter out) throws IOException
+	{
+		out.print("public ");
+		out.print(classData_.className);
+		out.print("(");
+		if (classData_.args != null)
+			out.print(classData_.args);
+		out.println(")");
+		out.beginBlock();
+		if (classData_.superArgs != null)
+		{
+			out.print("super(");
+			out.print(classData_.superArgs);
+			out.println(");");
+		}
+		else if ((classData_.superCall != null) || (classData_.superArgs != null))
+		{
+			out.print(classData_.superCall);
+			printSrcMapComment(out, classData_.superCall, classData_.superCallLine);
+		}
+		for (Argument arg : classData_.arguments)
+		{
+			arg.fieldAssign(out);
+			out.println();
+		}
+		out.endBlock();
+		out.println();
+		out.println();
+	}
+
+	
+	private void printClassPublicPrintMethod(SourceWriter out)
+	{
+		out.print("public synchronized void print(");
+		out.print(classData_.writerClassSimple);
+		out.print(" out)");
+		if (classData_.exception != null)
+		{
+			out.print(" throws ");
+			out.print(classData_.exception);
+		}
+		out.println();
+		out.beginBlock();
+			out.println("if (out == null)");
+			out.increaseTab();
+				out.println("throw new IllegalArgumentException(\"out is null\");");
+			out.decreaseTab();
+			out.println("this.out = out;");
+			if (!classData_.mixins.isEmpty())
+				out.println("init();");
+			out.println("print();");
+		out.endBlock();
+		out.println();
+		out.println();
+	}
+
+
+	private void printClassInitMethod(SourceWriter out)
+	{
+		if (!classData_.standalone)
+			out.print("@Override ");
+		out.println("protected void init()");
+		out.beginBlock();
+			out.println("super.init();");
+			for (MixinField mixin : classData_.mixins)
+			{
+				out.print(mixin.fieldName);
+				out.print(" = new ");
+				out.print(mixin.simpleName);
+				out.println("(out);");
+			}
+		out.endBlock();
+		out.println();
+		out.println();
+	}
+
+
+	private void printClassExitMethod(SourceWriter out)
+	{
+		if (!classData_.standalone)
+			out.print("@Override ");
+		out.println("protected void exit()");
+		out.beginBlock();
+			for (MixinField mixin : classData_.mixins)
+			{
+				out.print(mixin.fieldName);
+				out.println(" = null;");
+			}
+			out.println("super.exit();");
+		out.endBlock();
+		out.println();
+		out.println();
+	}
+	
+
+	private void printBody(String body)
+	{
+		int tab = out.getTabCount();
+		out.setTabCount(0);
+		out.print(body);
+		out.setTabCount(tab);
+	}
+	
+
+	private void printFields() throws IOException
+	{
+		if (classData_.hasFields())
+		{
+			out.println();
+			out.println();
+			for (Argument arg : classData_.arguments)
+			{
+				arg.fieldDecl(out);
+				out.println();
+			}
+			for (MixinField mixin : classData_.mixins)
+			{
+				out.print("protected ");
+				out.print(mixin.simpleName);
+				out.print(" ");
+				out.print(mixin.fieldName);
+				out.println(";");
+			}
+			if (classData_.standalone)
+			{
+				out.print("protected ");
+				out.print(classData_.writerClassSimple);
+				out.println(" out;");
+			}
+		}
+	}
+	
+	
+	private void printSrcMapComment(SourceWriter out, String s, int lineIndex)
+	{
+		if (printSrcMap_)
+		{
+			int column = out.getColumn();
+			for (int i=column; i<=70; i++)
+				out.print(' ');
+			out.print(" // line ");
+			out.print(lineIndex +  1);
+			out.print(": ");
+			out.print(s.trim());
+		}
+		out.println();
+	}
+
+
+	private final SourceWriter out;
+	private final ClassData classData_;
+	private final boolean printSrcMap_;
+}
--- src/java/org/civilian/tool/csp/CspCompiler.java
+++ src/java/org/civilian/tool/csp/CspCompiler.java
@@ -22,7 +22,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.StringWriter;
-import java.time.LocalDateTime;
 import java.util.HashMap;
 
 import org.civilian.template.ComponentBuilder;
@@ -355,15 +354,8 @@ public class CspCompiler
 			outBody.increaseTab();
 			compileJavaLines(outBody, true);
 
-			printClass(out, templFile);
-			int tab = out.getTabCount();
-			out.setTabCount(0);
-			out.print(swBody.toString());
-			out.setTabCount(tab);
-			if (classData_.hasFields())
-				printFields(out);
-
-			out.endBlock(); // class block, started in compileClassData
+			CspClassPrinter printer = new CspClassPrinter(out, classData_, options_.srcMap);
+			printer.print(templFile, options_.timestamp, swBody.toString());
 		}
 		out.flush();
 
@@ -375,197 +367,6 @@ public class CspCompiler
 	{
 		registeredMixins_.put(mixin.fieldName, mixin);
 		registeredMixins_.put(mixin.className, mixin);
-	}
-
-
-	private void printClass(SourceWriter out, File templFile) throws IOException
-	{
-		out.println("/**");
-		out.print(" * Generated from " + templFile.getName());
-		if (options_.timestamp)
-		{
-			out.print(" at ");
-			out.print(LocalDateTime.now());
-		}
-		out.println();
-		out.println(" * Do not edit.");
-		out.println(" */");
-		out.print("package ");
-		out.print(classData_.packageName);
-		out.println(";");
-		out.println();
-		out.println();
-		if (classData_.imports.write(out, null /* app package*/))
-		{
-			out.println();
-			out.println();
-		}
-		for (String prolog : classData_.prolog)
-			out.println(prolog);
-
-		if (classData_.isPublic)
-			out.print("public ");
-		if (classData_.isAbstract)
-			out.print("abstract ");
-		out.print("class ");
-		out.print(classData_.className);
-		if (classData_.extendsClass != null)
-		{
-			out.print(" extends ");
-			out.print(classData_.extendsClass);
-		}
-		if (classData_.implementsList != null)
-		{
-			out.print(" implements ");
-			out.print(classData_.implementsList);
-		}
-		out.println();
-		out.beginBlock();
-
-		if (classData_.needsCtor())
-			printClassCtor(out);
-
-		if (classData_.standalone)
-			printClassPublicPrintMethod(out);
-		if (!classData_.mixins.isEmpty())
-		{
-			printClassInitMethod(out);
-			printClassExitMethod(out);
-		}
-
-		if (classData_.hasMainTemplate)
-		{
-			if (classData_.extendsClass != null)
-				out.print("@Override ");
-			out.print("protected void print()");
-			if (classData_.exception != null)
-			{
-				out.print(" throws ");
-				out.print(classData_.exception);
-			}
-			out.println();
-		}
-	}
-
-
-	private void printClassCtor(SourceWriter out) throws IOException
-	{
-		out.print("public ");
-		out.print(classData_.className);
-		out.print("(");
-		if (classData_.args != null)
-			out.print(classData_.args);
-		out.println(")");
-		out.beginBlock();
-		if (classData_.superArgs != null)
-		{
-			out.print("super(");
-			out.print(classData_.superArgs);
-			out.println(");");
-		}
-		else if ((classData_.superCall != null) || (classData_.superArgs != null))
-		{
-			out.print(classData_.superCall);
-			printSrcMapComment(out, classData_.superCall, classData_.superCallLine);
-		}
-		for (Argument arg : classData_.arguments)
-		{
-			arg.fieldAssign(out);
-			out.println();
-		}
-		out.endBlock();
-		out.println();
-		out.println();
-	}
-
-
-	private void printClassPublicPrintMethod(SourceWriter out)
-	{
-		out.print("public synchronized void print(");
-		out.print(classData_.writerClassSimple);
-		out.print(" out)");
-		if (classData_.exception != null)
-		{
-			out.print(" throws ");
-			out.print(classData_.exception);
-		}
-		out.println();
-		out.beginBlock();
-			out.println("if (out == null)");
-			out.increaseTab();
-				out.println("throw new IllegalArgumentException(\"out is null\");");
-			out.decreaseTab();
-			out.println("this.out = out;");
-			if (!classData_.mixins.isEmpty())
-				out.println("init();");
-			out.println("print();");
-		out.endBlock();
-		out.println();
-		out.println();
-	}
-
-
-	private void printClassInitMethod(SourceWriter out)
-	{
-		if (!classData_.standalone)
-			out.print("@Override ");
-		out.println("protected void init()");
-		out.beginBlock();
-			out.println("super.init();");
-			for (MixinField mixin : classData_.mixins)
-			{
-				out.print(mixin.fieldName);
-				out.print(" = new ");
-				out.print(mixin.simpleName);
-				out.println("(out);");
-			}
-		out.endBlock();
-		out.println();
-		out.println();
-	}
-
-
-	private void printClassExitMethod(SourceWriter out)
-	{
-		if (!classData_.standalone)
-			out.print("@Override ");
-		out.println("protected void exit()");
-		out.beginBlock();
-			for (MixinField mixin : classData_.mixins)
-			{
-				out.print(mixin.fieldName);
-				out.println(" = null;");
-			}
-			out.println("super.exit();");
-		out.endBlock();
-		out.println();
-		out.println();
-	}
-	
-	
-	private void printFields(SourceWriter out) throws CspException, IOException
-	{
-		out.println();
-		out.println();
-		for (Argument arg : classData_.arguments)
-		{
-			arg.fieldDecl(out);
-			out.println();
-		}
-		for (MixinField mixin : classData_.mixins)
-		{
-			out.print("protected ");
-			out.print(mixin.simpleName);
-			out.print(" ");
-			out.print(mixin.fieldName);
-			out.println(";");
-		}
-		if (classData_.standalone)
-		{
-			out.print("protected ");
-			out.print(classData_.writerClassSimple);
-			out.println(" out;");
-		}
 	}
 
 
